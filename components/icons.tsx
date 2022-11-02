319808d2b6ead681eb753ce4063ecd826a0e04b8
obov <obov3e@gmail.com>
2022-11-03 06:08:27 +0900

    add two icons

--- components/icons.tsx
+++ components/icons.tsx
@@ -136,6 +136,59 @@ const Check = ({
     </div>
   );
 };
+const BookMark = ({
+  strokeWidth = 1.5,
+  iconClassName = "w-6 h-6",
+  ...props
+}: IconProps) => {
+  return (
+    <div {...props}>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+        strokeWidth={strokeWidth}
+        className={iconClassName}
+      >
+        <path d="M17.593 3.322c1.1.128 1.907 1.077 1.907 2.185V21L12 17.25 4.5 21V5.507c0-1.108.806-2.057 1.907-2.185a48.507 48.507 0 0111.186 0z" />
+      </svg>
+    </div>
+  );
+};
+const DownChevron = ({
+  strokeWidth = 1.5,
+  iconClassName = "w-6 h-6",
+  ...props
+}: IconProps) => {
+  return (
+    <div {...props}>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+        strokeWidth={strokeWidth}
+        className={iconClassName}
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          d="M19.5 8.25l-7.5 7.5-7.5-7.5"
+        />
+      </svg>
+    </div>
+  );
+};
 
-const icons = { LeftChevron, RightChevron, WarnRound, Plus, Minus, Check };
+const icons = {
+  LeftChevron,
+  RightChevron,
+  WarnRound,
+  Plus,
+  Minus,
+  Check,
+  BookMark,
+  DownChevron,
+};
 export default icons;
