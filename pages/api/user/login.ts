3afd48e86ddf1a9f87087931c28a1336a02f6679
obov <obov3e@gmail.com>
2022-11-03 12:15:06 +0900

    api ts error

--- pages/api/user/login.ts
+++ pages/api/user/login.ts
@@ -2,12 +2,12 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import client from "../../../libs/client";
 import jwt from "jsonwebtoken";
-import 'dotenv/config';
-import bcrypt from "bcryptjs"
-const secret_key = process.env.SECRET_KEY || ""
+import "dotenv/config";
+import bcrypt from "bcryptjs";
+const secret_key = process.env.SECRET_KEY || "";
 interface reqBody {
-  email: string | undefined
-  password: string | undefined
+  email: string | undefined;
+  password: string | undefined;
 }
 
 export default async function handler(
@@ -17,44 +17,35 @@ export default async function handler(
   const { email, password }: reqBody = req.body;
 
   try {
-    if(!email||!password){
-      throw new Error("아이디 혹은 비밀번호를 입력해 주세요")
+    if (!email || !password) {
+      throw new Error("아이디 혹은 비밀번호를 입력해 주세요");
     }
-    
-   const user = await client.user.findUnique({where:{email}});
-   const userId = user?.userId;
-   const encPass = user?.password;
 
-  if(userId == undefined){
-    res.status(400).send("이메일 또는 패스워드가 일치하지 않습니다.");
-  };
+    const user = await client.user.findUnique({ where: { email } });
+    const userId = user?.userId;
+    const encPass = user?.password;
 
-  if(encPass !== undefined){
-    const re = await bcrypt.compare(encPass, password);
-    if(!re){
+    if (userId == undefined) {
       res.status(400).send("이메일 또는 패스워드가 일치하지 않습니다.");
     }
-  }else {
-    res.status(400).send("존재하지 않는 유저입니다.");
-  }
-  
-  const accessToken = jwt.sign(
-    {userId},
-    secret_key,
-    { expiresIn: '1h' }
-  );
 
-  const refreshToken = jwt.sign(
-    {userId},
-    secret_key,
-    { expiresIn: '7d' }
-  );
-  
-  await client.user.update({where:{userId},data:{refreshToken}});
+    if (encPass !== undefined) {
+      const re = await bcrypt.compare(encPass, password);
+      if (!re) {
+        res.status(400).send("이메일 또는 패스워드가 일치하지 않습니다.");
+      }
+    } else {
+      res.status(400).send("존재하지 않는 유저입니다.");
+    }
 
-  res.setHeader('Set-Cookie', `accessToken: ${accessToken}`);
-  res.status(200).json({refreshToken});
+    const accessToken = jwt.sign({ userId }, secret_key, { expiresIn: "1h" });
 
+    const refreshToken = jwt.sign({ userId }, secret_key, { expiresIn: "7d" });
+
+    await client.user.update({ where: { userId }, data: { refreshToken } });
+
+    res.setHeader("Set-Cookie", `accessToken: ${accessToken}`);
+    res.status(200).json({ refreshToken });
   } catch (error) {
     res.status(400).json({ error, result: "로그인에 실패하였습니다." });
   }
