e92cbdd83c29a80975da62ebb952a5da149d3f81
obov <obov3e@gmail.com>
2022-11-03 09:08:29 +0900

    showindow data

--- components/Showwindow.tsx
+++ components/Showwindow.tsx
@@ -1,17 +1,21 @@
 import { Item } from "../types";
 import ItemCard from "./ItemCard";
 
-const Showwindow = () => {
-  const items: Item[] = Array(4).fill({
-    cartegory: "",
-    brand: "Asics",
-    name: "Asics x Jessica Gonsalves Gel-Nimbus 9 Cream White",
-    price: 205000,
-    isFast: false,
-    imgUrl:
-      "https://kream-phinf.pstatic.net/MjAyMjEwMjhfODgg/MDAxNjY2OTI1NDk5NzQz.QcnMiQ5-oS4nmIUgZ_fOntY7mH_EW5X5qN9prw1ktyIg.QcYJwAauPrOETxbcUT5aBlAfcNiJ_4M1E74towQlD8sg.PNG/a_879d30d9d24a429cac07d91ec6dddbb5.png?type=m_webp",
-    color: "bg-orange-200",
-  });
+interface Props {
+  items: Item[];
+}
+
+const Showwindow = ({ items }: Props) => {
+  // const items: Item[] = Array(4).fill({
+  //   cartegory: "",
+  //   brand: "Asics",
+  //   name: "Asics x Jessica Gonsalves Gel-Nimbus 9 Cream White",
+  //   price: 205000,
+  //   isFast: false,
+  //   imgUrl:
+  //     "https://kream-phinf.pstatic.net/MjAyMjEwMjhfODgg/MDAxNjY2OTI1NDk5NzQz.QcnMiQ5-oS4nmIUgZ_fOntY7mH_EW5X5qN9prw1ktyIg.QcYJwAauPrOETxbcUT5aBlAfcNiJ_4M1E74towQlD8sg.PNG/a_879d30d9d24a429cac07d91ec6dddbb5.png?type=m_webp",
+  //   color: "bg-orange-200",
+  // });
   return (
     <>
       <div className="w-[1280px] px-10 mx-auto">
